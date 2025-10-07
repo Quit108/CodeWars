4b8f92f5039979763507feb53fbdb2132c129d97
Quit108 <emontingelli@gmail.com>
2025-10-07 21:52:38 +0000

    add DoubleCharacter.py to double each character in a string add Summation.py to calculate the summation of numbers up to a given input

---
+++ 8th_lvl_Kata/DoubleCharacter.py
@@ -0,0 +1,6 @@
+s=input('Enter a string: ')
+
+def double_char(s):
+    return ''.join([c*2 for c in s])
+
+print(double_char(s))
---
+++ 8th_lvl_Kata/Summation.py
@@ -0,0 +1,6 @@
+num=int(input('Enter a number: '))
+
+def summation(num):
+    return sum(range(1, num + 1))
+
+print(summation(num))
